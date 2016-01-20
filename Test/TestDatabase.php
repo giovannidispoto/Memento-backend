cb2d95cfd09141894929c3adcc96a5603119877f
hageok <dispoto97@gmail.com>
2016-01-20 15:54:49 +0100

    Aggiunto test creazione utente => login => eliminazione utente e possibilità di eliminare utente

--- Test/TestDatabase.php
+++ Test/TestDatabase.php
@@ -12,28 +12,29 @@ class TestDatabase extends PHPUnit_Framework_TestCase
 {
 
 
-        public function testCreateUser(){
-            $db = new Database();
-            $res = $db->createUser("Giuseppe","Dispoto","prova@prova","giuseppedispoto","mariolino","01/07/1971","M");
-            $this->assertTrue($res);
-            $res = $db->createUser("Mario","Rossi","prova@prova.it","mariorossi","miao","10/07/1971","M");
-            $this->assertTrue($res);
+            public function testCreateUser(){ //test creazione utente
+                $db = new Database();
+                $res = $db->createUser("Giuseppe","Dispoto","prova@prova","giuseppedispoto","mariolino","01/07/1971","M");
+                $this->assertTrue($res);
+                $res = $db->createUser("Mario","Rossi","prova@prova.it","mariorossi","miao","10/07/1971","M");
+                $this->assertTrue($res);
 
 
-        }
-    public function testAuth(){
-        $db = new Database();
-        $res = $db->authUser("giuseppedispoto","mariolino");
-        $this->assertEquals($res,1);
-        $res = $db->authUser("utente32","admin123");
-        $this->assertEquals($res,0);
-    }
+            }
 
-    public function testDropUsers(){
-        $db = new Database();
-        $res = $db->dropUser("mariorossi");
-        $this->assertTrue($res);
-        $res = $db->dropUser("giuseppdispoto");
-        $this->assertTrue($res);
-    }
+            public function testAuth(){ //test autenticazione
+                $db = new Database();
+                $res = $db->authUser("giuseppedispoto","mariolino");
+                $this->assertEquals($res,1);
+                $res = $db->authUser("utente32","admin123");
+                $this->assertEquals($res,0);
+            }
+
+            public function testDropUsers(){// test cancellamento utenti
+                $db = new Database();
+                $res = $db->dropUser("mariorossi");
+                $this->assertTrue($res);
+                $res = $db->dropUser("giuseppdispoto");
+                $this->assertTrue($res);
+            }
 }
